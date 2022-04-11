39d89cc26392e29f24ae4278a109f61f6ee43fc4
ZHAOQI CHEN <z.chen5.21@abdn.ac.uk>
2022-04-11 14:12:31 +0000

    First commit

--- shop/models.py
+++ shop/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     amount = models.IntegerField()
     type = models.TextField(default='common')
-    created_date = models.DateTimeField(auto_now_add=False)
+    created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name, self.price, self.amount, self.type, self.created_date
