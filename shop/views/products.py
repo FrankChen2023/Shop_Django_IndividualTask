f2e3f3f01444714965b00c7f40c6729dffdf9f27
ZHAOQI CHEN <z.chen5.21@abdn.ac.uk>
2022-04-11 13:32:41 +0000

    First commit

--- shop/views/products.py
+++ shop/views/products.py
@@ -4,6 +4,7 @@ from shop.models import Product
 from django.contrib.auth.decorators import login_required
 
 def product_list(request):
+    Product.objects.create(name='banana', price='3.99', amount='99', type='Food').save()
     products = Product.objects.all()    
     return render(request, 'shop/product_list.html', {'products' : products })
 
@@ -11,9 +12,9 @@ def product_list(request):
 # @login_required
 def product_add(request):
     if request.method=="POST":
-        name = request.POST.get('name','apple')
-        price = request.POST.get('price','1.99')
-        amount = request.POST.get('amount','99')
-        type = request.POST.get('type','Food')
+        name = request.POST.get('name','')
+        price = request.POST.get('price','')
+        amount = request.POST.get('amount','')
+        type = request.POST.get('type','')
         Product.objects.create(name=name, price=price, amount=amount, type=type).save()
     return render(request, 'shop/product_add.html')
