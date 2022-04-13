5367467df46696afc3b73f3ddccc392288aa4ba2
ZHAOQI CHEN <z.chen5.21@abdn.ac.uk>
2022-04-13 08:17:32 +0000

    add product search function

--- shop/urls.py
+++ shop/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
+        path('', views.products.product_search, name='product_search'),
         path('product_add/', views.products.product_add, name='product_add'),
-        path('', views.products.product_list, name='product_list'),
-
+        path('product_list/', views.products.product_list, name='product_list'),
 ]
--- shop/views/products.py
+++ shop/views/products.py
@@ -19,3 +19,15 @@ def product_add(request):
         product = Product.objects.create(name=name, price=price, amount=amount, type=type)
         product.save()
     return render(request, 'shop/product_add.html', {'product' : product})
+
+def product_search(request):
+    product = Product.objects.all()
+    if request.method=="POST":
+        key = request.POST.get('key', 'name')
+        target = request.POST.get('target', '')
+        if key=='id':
+            product = Product.objects.filter(id=target)
+        if key=='name':
+            product = Product.objects.filter(name__icontains=target)
+        return render(request, 'shop/product_search.html', {'product' : product})
+        
