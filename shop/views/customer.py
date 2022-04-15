510c27fb07d4f5f714ef29d5f010dcea9bf7133f
ZHAOQI CHEN <z.chen5.21@abdn.ac.uk>
2022-04-15 21:25:40 +0000

    balance function added

--- shop/views/customer.py
+++ shop/views/customer.py
@@ -6,6 +6,6 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def customer_balance(request):
     username = request.user.username
-    current_user = Customer.objects.filter(username=username)
+    current_user = Customer.objects.get(username=username)
     baskets = Basket.objects.filter(username=username)
     return render(request, 'shop/customer_balance.html', {'current_user' : current_user, 'baskets' : baskets})
