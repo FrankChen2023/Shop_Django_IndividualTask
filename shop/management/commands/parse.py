d5be9e5ef17ceca52ad77426eb711e32b977f9b3
ZHAOQI CHEN <z.chen5.21@abdn.ac.uk>
2022-05-05 13:26:00 +0000

    Product added

--- shop/management/commands/parse.py
+++ shop/management/commands/parse.py
@@ -10,18 +10,9 @@ class Command(BaseCommand):
     help = 'Load data into the tables'
 
     def handle(self, *args, **options):
-        base_dir = Path(__file__).resolve().parent.parent.parent.parent
-        with open(str(base_dir) + '/shop/data.csv', newline='') as f:
-            reader = csv.reader(f, delimiter=",")
-            next(reader) # skip the header line
-            type = 'common'
-            for row in reader:
-                print(row)
-                amount = random.randrange(20,999)
-                try:
-                    Product.objects.create(name=row[0], type=type, amount=amount, price=row[1]).save()
-                except:
-                    pass
+        
+        Product.objects.filter(name='').delete()
+        Product.objects.filter(price=0.00).delete()
                 
         print("data parsed successfully")
 
